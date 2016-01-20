4925b480d223779f2e0b2e2ff70472d4e6d84106
Zak Kolar <zak.kolar@gmail.com>
2016-01-20 17:10:35 -0500

    Fixed timezone issue with date picker

--- dh/config/application.rb
+++ dh/config/application.rb
@@ -23,5 +23,7 @@ module Dh
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = 'Eastern Time (US & Canada)'
   end
 end
